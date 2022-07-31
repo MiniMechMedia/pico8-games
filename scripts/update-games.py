79a5ad7f790349aea9fbafb063bf96d0ffe23f77
Caterpillar Games <noreply@caterpillargames.itch.io>
2022-07-31 14:08:26 -0400

    Try it

--- scripts/update-games.py
+++ scripts/update-games.py
@@ -3,11 +3,13 @@ import subprocess
 import traceback
 import glob
 import shutil
+import os
 
 try:
 	abs_file = pathlib.Path(__file__).absolute()
 	git_repo_root = abs_file.parent.parent.resolve()
 
+	print('Updating git repo')
 	subprocess.run(
 		'git pull origin master'.split(),
 		cwd=str(git_repo_root),
@@ -18,6 +20,7 @@ try:
 
 	gameListXml = '<gameList>'
 
+	print('Updating gamelist.xml')
 	games = glob.glob(str(git_repo_root / 'carts/*/export/*.png'))
 	print(f'found {len(games)} games')
 	for game in games:
@@ -31,6 +34,38 @@ try:
 
 	with open('/opt/retropie/configs/all/emulationstation/gamelists/pico8/gamelist.xml', 'w') as f:
 		f.write(gameListXml)
+
+	print('Updating pico-8')
+	downloads_root = git_repo_root.parent / 'downloads'
+	try:
+		shutil.rmtree(str(downloads_root))
+	except:
+		pass
+	os.mkdir(str(downloads_root))
+
+	subprocess.run(
+		'wget https://www.lexaloffle.com/dl/7tiann/pico-8_0.2.4_raspi.zip'.split(),
+		cwd=str(downloads_root),
+		check=True
+		)
+
+	for file in glob.glob(str(downloads_root / '*')):
+		subprocess.run(
+			[
+				'unzip', 
+				file
+			],
+			cwd=str(downloads_root),
+			check=True
+		)
+
+	try:
+		os.remove('/home/pi/pico-8/pico8_dyn')
+	except:
+		pass
+
+	shutil.copy(downloads_root / 'pico-8' / 'pico8_dyn', '/home/pi/pico-8')
+
 
 except:
 	# traceback.print_exc()
