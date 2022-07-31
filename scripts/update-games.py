9ed38550074d1b3a5c3a00bdebe36e6f4d602d99
Caterpillar Games <noreply@caterpillargames.itch.io>
2022-07-31 13:21:56 -0400

    Adding update script

---
+++ scripts/update-games.py
@@ -0,0 +1,36 @@
+import pathlib
+import subprocess
+import traceback
+import glob
+import shutil
+
+try:
+	git_repo_root = pathlib.Path(__file__).parent.parent.resolve()
+
+	subprocess.run(
+		'git pull origin master'.split(),
+		cwd=str(git_repo_root)
+		)
+
+	games_root = git_repo_root.parent / 'games'
+
+	gameListXml = '<gameList>'
+
+	games = glob.glob(git_repo_root / '/carts/*/export/*.png')
+	for game in games:
+		print(game)
+		shutil.copy(game, games_root)
+		gamePath = pathlib.Path(game)
+		with open(gamePath.parent / 'game.xml') as gameXml:
+			gameListXml += gameXml.read()
+
+	gameListXml += '</gameList>'
+
+	with open('/opt/retropie/configs/all/emulationstation/gamelists/pico8/gamelist.xml', 'w') as f:
+		f.write(gameListXml)
+
+except:
+	traceback.print_exc()
+
+print('Press enter to continue')
+input()
