8274dff56588a2dbe29514999ef5a477f6c99946
Caterpillar Games <noreply@caterpillargames.itch.io>
2022-12-11 13:11:05 -0500

    Have my pipeline working. Just need to write the thing to display the image

--- carts/the-golem/convert.py
+++ carts/the-golem/convert.py
@@ -8,6 +8,7 @@ import os
 # Open the PNG image and read the pixel data
 img_path = sys.argv[1]
 img_filename = img_path.split('/')[-1]
+img_filebase = img_filename.split('.png')[0]
 image = Image.open(img_path).convert('RGB')
     # .convert('L') \
     
@@ -84,11 +85,37 @@ for y in range(height):
         color = color_map[pixel]
         hex_val = hex(color)
         hex_string += hex_val[-1]
+    hex_string += '\n'
 hex_string = f'"{hex_string}"'
 # Print the hex string
-print(hex_string)
+# print(hex_string)
+
+with open('compression/px9.p8', 'r') as compression_template:
+    original = compression_template.read()
+    front, back = original.split('__gfx__')
+    _, back = original.split('__label__')
+    final = f'{front}__gfx__\n{hex_string}__label__\n{back}'
+
+compression_cart_filename = f'px9_{img_filebase}.p8'
+with open(f'compression/{compression_cart_filename}', 'w') as compression_cart:
+    compression_cart.write(final)
+
+try:
+    os.remove('output.bin.p8l')
+except FileNotFoundError:
+    pass
+
+os.system(f'/Applications/PICO-8.app/Contents/MacOS/pico8 -x compression/{compression_cart_filename}')
+import time
+time.sleep(3)
+
+with open('output.bin.p8l', 'r') as file:
+    compressed_escaped_string = file.read().strip()
+
+with open(f'_img_{img_filebase}.lua', 'w') as file:
+    file.write(f'_img_{img_filebase} = "{compressed_escaped_string}"')
 
 
-# Copy the text to the clipboard
-print('has been copied to clipboard')
-pyperclip.copy(hex_string)
+# # Copy the text to the clipboard
+# print('has been copied to clipboard')
+# pyperclip.copy(hex_string)
