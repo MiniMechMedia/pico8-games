85394908df3c4936d3b75abc3d83847867523a02
Caterpillar Games <noreply@caterpillargames.itch.io>
2024-04-23 19:57:34 -0400

    oh that is wrong too

--- carts/graphics-with-pico8/slide_050_2d_transform_rot.lua
+++ carts/graphics-with-pico8/slide_050_2d_transform_rot.lua
@@ -22,21 +22,18 @@ function draw()
             {
                 scale={x=.5, y=.25},
                 pos={x=.5,y=0},
-                rot=0.25
+                rot=0.125
             }
         )
     }
 
     for obj in all(objects) do
         for vertex in all(obj.mesh) do
-            obj_x = vertex.x * obj.scale.x
-            obj_y = vertex.y * obj.scale.y
+            obj_x = vertex.x * cos(obj.rot) - vertex.y * sin(obj.rot)
+            obj_y = vertex.x * sin(obj.rot) + vertex.y * cos(obj.rot)
 
-            obj_x = obj_x * cos(obj.rot) - obj_y * sin(obj.rot)
-            obj_y = obj_x * sin(obj.rot) + obj_y * cos(obj.rot)
-
-            world_x = obj_x + obj.pos.x
-            world_y = obj_y + obj.pos.y
+            world_x = obj_x * obj.scale.x + obj.pos.x
+            world_y = obj_y * obj.scale.y + obj.pos.y
 
             screen_x = world_x * SCALE + OFFSET
             screen_y = world_y * SCALE + OFFSET
