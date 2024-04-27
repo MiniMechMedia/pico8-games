f700f5d7f4f35f60a3ec3e5a044d9b8d8528dace
Caterpillar Games <noreply@caterpillargames.itch.io>
2024-04-26 22:29:26 -0400

    well this sucks

--- carts/graphics-with-pico8/slide_107_cube_solid_faces_one_face_solid.lua
+++ carts/graphics-with-pico8/slide_107_cube_solid_faces_one_face_solid.lua
@@ -26,16 +26,11 @@ function draw()
 
             local a2 = face[4]
             local b2 = face[3]
-            -- a2,b2 = b2,a2
             local dx2 = b2.x - a2.x
             local dy2 = b2.y - a2.y
             local dz2 = b2.z - a2.z
 
-            -- for vertex in all(face) do
-            --     -- line(vertex.x*32+64, vertex.y*32+64)
-            --     line(obj:objToScreen(vertex))
-            -- end
-            for i = 0, 1, 0.02 do
+            for i = 0, 1, 0.03 do
                 local start = {
                     x = a1.x + dx1*i,
                     y = a1.y + dy1*i,
@@ -52,18 +47,59 @@ function draw()
                 finish_x, finish_y = obj:objToScreen(finish)
                 
                 line(start_x, start_y, finish_x, finish_y)
+
+                i+=0.1
+                local finish = {
+                    x = a2.x + dx2*i,
+                    y = a2.y + dy2*i,
+                    z = a2.z + dz2*i,
+                }              
+                finish_x, finish_y = obj:objToScreen(finish)
+                line(start_x, start_y, finish_x, finish_y)
+
             end
 
-            -- for vertex in all(face) do
-            --     line(obj:objToScreen(vertex))
-            -- end
 
-                        -- rotated = rotate(vertex, obj.rot)
-            -- world_x, world_y, world_z = rotated.x, rotated.y, rotated.z
+            local a1 = face[1]
+            local b1 = face[4]
+            local dx1 = b1.x - a1.x
+            local dy1 = b1.y - a1.y
+            local dz1 = b1.z - a1.z
 
-            -- screen_x = world_x * SCALE + OFFSET
-            -- screen_y = world_y * SCALE + OFFSET
-            
+            local a2 = face[2]
+            local b2 = face[3]
+            local dx2 = b2.x - a2.x
+            local dy2 = b2.y - a2.y
+            local dz2 = b2.z - a2.z
+
+            for i = 0, 1, 0.03 do
+                local start = {
+                    x = a1.x + dx1*i,
+                    y = a1.y + dy1*i,
+                    z = a1.z + dz1*i,
+                }
+
+                start_x, start_y = obj:objToScreen(start)
+
+                local finish = {
+                    x = a2.x + dx2*i,
+                    y = a2.y + dy2*i,
+                    z = a2.z + dz2*i,
+                }              
+                finish_x, finish_y = obj:objToScreen(finish)
+                
+                line(start_x, start_y, finish_x, finish_y)
+
+                i+=0.1
+                local finish = {
+                    x = a2.x + dx2*i,
+                    y = a2.y + dy2*i,
+                    z = a2.z + dz2*i,
+                }              
+                finish_x, finish_y = obj:objToScreen(finish)
+                line(start_x, start_y, finish_x, finish_y)
+
+            end
 
         end
     end
